dbaf05674c93dbca2486ed5eaf5334ec9c6b5d69
jiang_30 <514770300@qq.com>
2020-09-23 07:43:35 +0800

    d

--- src/index.js
+++ src/index.js
@@ -1,5 +1,58 @@
 /**
+ * @file fjalf asjdfl 
+ * @author - jiang - mail qq.com
+ * @description lfjsdlkjf f jlf3 333 
+ *
+ */
+/**
  * Student Name
  * @type {string}
  */
-const studentName = 1 
+const studentName = 'jiang'
+
+/**
+ * Person Class
+ * 
+ */
+class Person {
+  /**
+   * 狗爪函数
+   * @param { {name: string} } info 个人基本信息
+   */
+  constructor(info){
+
+  }
+
+  /**
+   * prop jiang
+   */
+  name = 'jiang'
+
+  /**
+   * eat function
+   * @returns { {name: string, age: number} } info
+   */
+  eat(){
+    return {name: '', age: 20}
+  }
+}
+
+/**
+ * 
+ */
+
+/**
+ * @param { callbackRe } cb 
+ */
+function doSome(cb){
+  cb('a', 23)
+}
+
+/**
+ * @callback callbackRe
+ * @param {string} a 
+ * @param {number} b 
+ */
+function a(a, b, c){
+
+}
